8a548e64e15db793a8f75760d70fd6c02fbb8b82
Felix <barosiofelix@gmail.com>
2022-08-21 14:14:23 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // your code here
+  const todo = document.querySelector('#tasks');
+  const newTask = document.querySelector('#new-task-description');
+  const form = document.querySelector('#create-task-form');
+
+  const myInput = (input) => {
+    return input ? input : false;
+  };
+
+  const addTask = (e) => {
+    e.preventDefault();
+    myNewTask();
+  };
+
+  const myNewTask = () => {
+    const task = newTask.value;
+    if (myInput(task)) {
+      createNewTask(task);
+    }
+  };
+
+  const createNewTask = (content) => {
+    const task = document.createElement('li');
+    task.textContent = content;
+    todo.appendChild(task);
+    form.reset();
+  };
+
+  form.addEventListener('submit', addTask);
 });
